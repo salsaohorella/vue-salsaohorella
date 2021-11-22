c5dfbb29743e3a82dbd93b58901b0081deeeb2e5
Salsa Darmayani Ohorella <93893556+salsaohorella@users.noreply.github.com>
2021-11-22 11:47:28 +0700

    Publish

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/vue-salsaohorella/" : "/"
+    publicPath: process.NODE_ENV === "production" ? "/vue-salsaohorella/" : "/"
 } 
